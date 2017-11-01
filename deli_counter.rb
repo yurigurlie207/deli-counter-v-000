888ba197cf4ec7fe10109fee15d51fde0a625142
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-01 17:18:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,6 +15,13 @@ def line(katz_deli)
   end
 end
 
+# print "The line is currently:"
+# count = 1;
+# katz_deli.each do |name|
+#   print " #{count}. #{name}"
+#   count +=1
+# end
+
 def take_a_number(katz_deli,name)
     katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
