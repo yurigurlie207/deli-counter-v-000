1725f6e29daf29c918f9c9f3227fea37aecaf008
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-01 17:02:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,13 @@
 # Write your code here.
 katz_deli = []
+
 def line(katz_deli)
-  
+
   if katz_deli == [] then
     "The line is currently empty."
   end
-  
+
+end
+
+def take_a_number(katz_deli,name)
 end
