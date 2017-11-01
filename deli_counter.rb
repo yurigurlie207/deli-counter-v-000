f158da8de4868c426ca586f4672db686de7d164b
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-01 17:19:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -29,7 +29,7 @@ def take_a_number(katz_deli,name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli == [] then
+  if katz_deli == [] || katz_deli == nil then
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}.";
