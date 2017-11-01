841b79c575048390d54e032b9afdb21c29406264
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-01 17:20:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -33,6 +33,6 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}.";
-    katz_deli.pop
+    katz_deli.shift
   end
 end
